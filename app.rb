6aa2de310769aa0a1d7f3b102e69afdce1e25d6f
Drew Kitch <drew.kitch@gmail.com>
2015-12-30 02:23:09 -0700

    present results of scan with each country code inline

--- app.rb
+++ app.rb
@@ -12,36 +12,40 @@ get '/' do
 !!! 5
 %html
   %head
+    :css
+      table, th, td {
+        border-collapse: collapse;
+        border: 1px solid black;
+      }
+      td > img {
+        padding: 4px 0 0 5px;
+      }
     %title VIES Service Tracking
   %body
-    %h2 Statuses
-    - country_status_lists.each do |country_code, statuses|
-      %h3= eu_countries[country_code]
-      %table
+    %h2 Outage Scans
+    %table
+      %tr
+        %th Scanned at
+        - eu_countries.keys.each do |country_code|
+          %th= country_code
+      - outage_scans.each do |scanned_at, statuses|
         %tr
-          %th{:style => "padding-right:30px;"} ws access
-          %th{:style => "padding-right:20px;"} web access
-          %th outage noted at
-        - statuses.each do |status|
-          %tr
-            %td{:style => "text-align:center;"}
-              - img_src = status.ws_access ? "img/bullet-green.png" : "img/bullet-red.png"
+          %td= scanned_at
+          - eu_countries.keys.each do |country_code|
+            - status = statuses.find{|status| country_code == status.country_code.upcase}
+            %td
+              - img_src = status ? "img/bullet-red.png" : "img/bullet-green.png"
               %img{:src => img_src, :height => 15, :width => 15}
-            %td{:style => "text-align:center;"}
-              - img_src = status.web_access ? "img/bullet-green.png" : "img/bullet-red.png"
-              %img{:src => img_src, :height => 15, :width => 15}
-            %td{:style => "text-align:center;"}= status.created_at
-
 TEM
 
   seven_days_ago = Time.now.utc - (3600 * 24 * 7)
-  ws_access_failed_statuses = Status.all(:created_at.gte => seven_days_ago, :ws_access => false)
+  ws_access_failed_statuses = Status.all(:created_at.gte => seven_days_ago, :ws_access => false).reverse
 
-  country_status_lists = {}
+  outage_scans = {}
   ws_access_failed_statuses.each do |status|
-    country_status_lists[status.country_code] ||= []
-    country_status_lists[status.country_code] << status
+    outage_scans[status.created_at] ||= []
+    outage_scans[status.created_at] << status
   end
 
-  Haml::Engine.new(template).render(Object.new, country_status_lists: country_status_lists, eu_countries: EU_COUNTRIES)
+  Haml::Engine.new(template).render(Object.new, outage_scans: outage_scans, eu_countries: EU_COUNTRIES)
 end

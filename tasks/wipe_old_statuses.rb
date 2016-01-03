dbe9d978d3f37c128c33fac007a32a6643ecec23
Drew Kitch <drew.kitch@gmail.com>
2016-01-02 22:00:00 -0700

    stay under heroku 10k record limit

--- tasks/wipe_old_statuses.rb
+++ tasks/wipe_old_statuses.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
 require_relative '../status'
+
+# limit to last 7 days
 Status.all(:created_at.lt => (Time.now.utc - (3600 * 24 * 7))).destroy
+
+# stay under heroku 10k rows limit
+if Status.count > 9000
+  Status.first(1000).destroy
+end

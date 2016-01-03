38e49f4072fe48b09926ecd05efdfb7d4ac94faa
Drew Kitch <drew.kitch@gmail.com>
2016-01-02 21:39:18 -0700

    leave out ws_access check on db query, as failed state is a given

--- app.rb
+++ app.rb
@@ -69,7 +69,7 @@ get '/country' do
 TEM
 
   seven_days_ago = Time.now.utc - (3600 * 24 * 7)
-  ws_access_failed_statuses = Status.all(:country_code => country_code, :ws_access => false, :created_at.gte => seven_days_ago).reverse
+  ws_access_failed_statuses = Status.all(:country_code => country_code, :created_at.gte => seven_days_ago).reverse
 
   Haml::Engine.new(template).render(Object.new, outage_scans: ws_access_failed_statuses, country_code: country_code, eu_countries: EU_COUNTRIES)
 end

2e752e976ce9bec85b6613d08047e54eeff74611
Drew Kitch <drew.kitch@gmail.com>
2016-02-07 16:53:54 -0700

    change hard limit from 7 to 14 days

--- tasks/wipe_old_statuses.rb
+++ tasks/wipe_old_statuses.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require_relative '../status'
 
-# limit to last 7 days
-Status.all(:created_at.lt => (Time.now.utc - (3600 * 24 * 7))).destroy
+# limit to last 14 days
+Status.all(:created_at.lt => (Time.now.utc - (3600 * 24 * 14))).destroy
 
 # stay under heroku 10k rows limit
 if Status.count > 9000
